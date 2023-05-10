7443e0c07c69d4c4e10f985c395980640e4143fd
brandontnavarrete <116035881+brandontnavarrete@users.noreply.github.com>
2023-05-10 01:25:57 +0000

    adding consumable class and implementing

--- main.py
+++ main.py
@@ -1,71 +1,84 @@
 from player import Player
 from weapon import Weapon
+from consumable import Consumable
 from insect import Insect
 
-# create a player with high health
+# Create a new player
 player_name = input("Enter your name: ")
-player = Player(player_name, 100)
+player = Player(player_name)
 
-# create a list of available weapons
-available_weapons = [
-    Weapon("Sword", 15, 10),
-    Weapon("Axe", 20, 8),
-    Weapon("Mace", 10, 12),
-    Weapon("Bow", 12, 15)
-]
+# Create some weapons
+sword = Weapon("Sword", 10, 10)
+axe = Weapon("Axe", 8, 12)
+dagger = Weapon("Dagger", 6, 8)
 
-# prompt the player to choose a weapon
-print("Available weapons:")
-for i, weapon in enumerate(available_weapons):
-    print(f"{i+1}. {weapon.name} ({weapon.damage} strength, {weapon.durability} durability)")
-weapon_choice = int(input("Choose a weapon (1-4): "))
-player.get_weapon(available_weapons[weapon_choice-1])
-print(f"You have chosen {player.weapon.name} as your weapon.")
+# Let the player choose a weapon
+print("Choose a weapon:")
+print("1. Sword")
+print("2. Axe")
+print("3. Dagger")
+weapon_choice = input("Enter the number of the weapon you want to choose: ")
+if weapon_choice == "1":
+    player.get_weapon(sword)
+elif weapon_choice == "2":
+    player.get_weapon(axe)
+elif weapon_choice == "3":
+    player.get_weapon(dagger)
+else:
+    print("Invalid choice. No weapon equipped.")
 
-# simulate an endless wave of insect attacks
-insect_wave = 1
-insect_count = 5
-score = 0
-while True:
-    print(f"\nInsect wave {insect_wave} - {insect_count} insects remaining.")
-    # create a list of insects for this wave
-    insects = [Insect(f"Insect {i+1}") for i in range(insect_count)]
-    # attack each insect until it's defeated or the player dies
-    for insect in insects:
-        print(f"\n{insect.name} appears!")
-        while insect.health > 0 and player.health > 0:
-            print(f"\nYour health: {player.health}  {insect.name} health: {insect.health}")
-            attack_choice = input("Do you want to attack? (y/n): ")
-            if attack_choice.lower() == 'y':
-                damage = player.weapon.damage
-                insect.take_damage(damage)
-                print(f"You hit {insect.name} for {damage} damage!")
-                if insect.health <= 0:
-                    insect_count -= 1
-                    score += 1
-                    print(f"You defeated {insect.name}!")
-                    break
-                damage = insect.attack(player)
-                player.take_damage(damage)
-                print(f"{insect.name} hit you for {damage} damage!")
-            else:
-                print("You chose not to attack.")
-        if player.health <= 0:
-            break
-    # increase the strength of the insects for the next wave
-    insect_wave += 1
-    insect_count += 2
-    for insect in insects:
-        insect.health += 10
-    # check if the player is defeated
+# Create some consumables
+apple = Consumable("Apple", 10)
+potion = Consumable("Potion", 25)
+
+# Let the player use a consumable
+print("Use a consumable:")
+print("1. Apple (+10 health)")
+print("2. Potion (+25 health)")
+consumable_choice = input("Enter the number of the consumable you want to use, or enter 0 to skip: ")
+if consumable_choice == "1":
+    player.use_consumable(apple)
+elif consumable_choice == "2":
+    player.use_consumable(potion)
+else:
+    print("No consumable used.")
+
+# Start the battle with an insect
+insect = Insect("Insect")
+print(f"A wild {insect.name} appears!")
+
+# Loop until the player or insect is defeated or the player runs away
+while player.health > 0 and insect.health > 0:
+    # Prompt the player to attack or use a consumable
+    print("Choose an action:")
+    print("1. Attack")
+    print("2. Use a consumable")
+    action_choice = input("Enter the number of the action you want to take: ")
+    if action_choice == "1":
+        player.attack(insect)
+    elif action_choice == "2":
+        print("Use a consumable:")
+        print("1. Apple (+10 health)")
+        print("2. Potion (+25 health)")
+        consumable_choice = input("Enter the number of the consumable you want to use: ")
+        if consumable_choice == "1":
+            player.use_consumable(apple)
+        elif consumable_choice == "2":
+            player.use_consumable(potion)
+        else:
+            print("Invalid choice. Turn skipped.")
+    else:
+        print("Invalid choice. Turn skipped.")
+        
+    # Check if the insect is defeated
+    if insect.health <= 0:
+        print(f"{insect.name} has been defeated!")
+        break
+        
+    # Insect attacks
+    insect.attack(player)
+    
+    # Check if the player is defeated
     if player.health <= 0:
-        print("\nYou have been defeated!")
+        print(f"{player.name} has been defeated!")
         break
-    # check if the player has defeated 5 waves and end the game
-    if score >= 5:
-        print("\nYou have defeated 5 waves of insects and survived!")
-        break
-
-# display the final score
-print(f"\nYour final score is {score}.")
-n
--- test.py
+++ test.py
@@ -22,17 +22,18 @@ weapon_choice = int(input("Choose a weapon (1-4): "))
 player.get_weapon(available_weapons[weapon_choice-1])
 print(f"You have chosen {player.weapon.name} as your weapon.")
 
-# simulate a wave of insect attacks
+# simulate an endless wave of insect attacks
 insect_wave = 1
 insect_count = 5
-while player.health > 0 and insect_count > 0:
+score = 0
+while True:
     print(f"\nInsect wave {insect_wave} - {insect_count} insects remaining.")
     # create a list of insects for this wave
     insects = [Insect(f"Insect {i+1}") for i in range(insect_count)]
-    # attack each insect until it's defeated or the player dies
+    # attack each insect until it'ys defeated or the player dies
     for insect in insects:
         print(f"\n{insect.name} appears!")
-        while insect.health > 0:
+        while insect.health > 0 and player.health > 0:
             print(f"\nYour health: {player.health}  {insect.name} health: {insect.health}")
             attack_choice = input("Do you want to attack? (y/n): ")
             if attack_choice.lower() == 'y':
@@ -41,27 +42,30 @@ while player.health > 0 and insect_count > 0:
                 print(f"You hit {insect.name} for {damage} damage!")
                 if insect.health <= 0:
                     insect_count -= 1
+                    score += 1
                     print(f"You defeated {insect.name}!")
                     break
                 damage = insect.attack(player)
                 player.take_damage(damage)
                 print(f"{insect.name} hit you for {damage} damage!")
-                if player.health <= 0:
-                    break
             else:
                 print("You chose not to attack.")
+        if player.health <= 0:
+            break
     # increase the strength of the insects for the next wave
     insect_wave += 1
     insect_count += 2
     for insect in insects:
         insect.health += 10
-    # end the game if the player is defeated
+    # check if the player is defeated
     if player.health <= 0:
         print("\nYou have been defeated!")
         break
+    # check if the player has defeated 5 waves and end the game
+    if score >= 5:
+        print("\nYou have defeated 5 waves of insects and survived!")
+        break
 
 # display the final score
-if player.health > 0:
-    print("\nYou survived all the insect waves!")
-else:
-    print(f"\nGame over. {player.name} survived {insect_wave-1} waves and defeated {5-insect_count} insects.") 
+print(f"\nYour final score is {score}.")
+n
